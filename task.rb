c1bf313741036b6533383e04fc20ad6d445d77c4
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-30 18:14:18 +0700

    Окончательное решение

--- task.rb
+++ task.rb
@@ -11,26 +11,17 @@
 # > ruby task.rb 1 0 -4
 # > 2.00 -2.00
 
-
 A, B, C = ARGV.map(&:to_f)
 D = B * B - 4 * A * C
 
-def no_roots
-  'NO ROOTS'
+if D.zero?
+  puts format('%.2f', (-B / 2 / A).round(2))
+else
+  if D > 0 then
+    x1 = (-B - Math.sqrt(D)) / 2 / A
+    x2 = (-B + Math.sqrt(D)) / 2 / A
+    puts "#{format('%.2f', x2.round(2))} #{format('%.2f', x1.round(2))}"
+  else
+    puts 'NO ROOTS'
+  end
 end
-
-def one_root
-  -B / 2 / A
-end
-
-def two_roots
-  x1 = (-B - Math.sqrt(D)) / 2 / A
-  x2 = (-B + Math.sqrt(D)) / 2 / A
-  "#{x1.round(2)} #{x2.round(2)}"
-end
-
-puts result = if D.zero?
-                one_root
-              else
-                D > 0 ? two_roots : no_roots
-              end
